f496740326ce7dab1a85427625d164ab43492cd6
OpenShift guest <anibal.souza@gmail.com>
2014-07-07 10:43:10 -0400

    Initial Release

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
- echo "Hello world - Teste3";
+<?php // Example 26-4: index.php
+  require_once 'header.php';
+
+  echo "<br><span class='main'>Welcome to $appname,";
+
+  if ($loggedin) echo " $user, you are logged in.";
+  else           echo ' please sign up and/or log in to join in.';
 ?>
+
+    </span><br><br>
+  </body>
+</html>
